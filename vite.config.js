04e42a78ebd49caebc0f7b6a25d0076848fd3a01
daytoywhy <1320007006@qq.com>
2024-03-04 16:14:43 +0800

    代码提交

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,24 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import requireTransform from 'vite-plugin-require-transform';
 
+const express = require('express')
+// import { registerRouter } from './backend/router.js'
+// console.log(registerRouter,'数据');
+const registerRouter = require('./backend/router.js')
+
+
+async function createServer(){
+  const app = express()
+  registerRouter(app)
+  app.listen(3000, function(err) {
+    if (err) {
+      console.log(err)
+      return
+    }
+    console.log('Listening at http://localhost:3000' + '\n')
+  })
+}
+createServer()
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -29,6 +47,7 @@ export default defineConfig({
     }
   },
   server:{
+    open: './server.js',
     proxy:{
       '/api':{
         target:  'http://localhost:3000',
